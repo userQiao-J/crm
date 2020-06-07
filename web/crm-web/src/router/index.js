cd266356e516f2bd77a3e27caad1d1eec07be31d
userqiao <userqiao@163.com>
2020-06-07 17:37:31 +0800

    左侧导航菜单制作

--- web/crm-web/src/router/index.js
+++ web/crm-web/src/router/index.js
@@ -2,19 +2,31 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
 import Home from "../views/Home.vue";
-
+import Test from "../views/Test.vue";
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
     name: "Login",
-    component: Login
+    component: Login,
+    hidden: true
   },
   {
     path: "/home",
     name: "Home",
-    component: Home
+    component: Home,
+    hidden: true
+  },
+  {
+    path: "/home",
+    name:"导航一",
+    component: Home,
+    children: [{
+      path:'/test',
+      name:'TEST页面',
+      component: Test
+    }]
   }
 ];
 
--- web/crm-web/src/views/Home.vue
+++ web/crm-web/src/views/Home.vue
@@ -5,7 +5,7 @@
         <div class="title">CRM客户管理系统</div>
         <el-dropdown class="userInfo" @command="commandHandler">
           <span class="el-dropdown-link">
-            {{ user.name }}<i><img :src="user.userface" alt="头像"></i>
+            {{ user.name }}<i><img :src="user.userface" alt="头像"/></i>
           </span>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item command="userinfo">个人中心</el-dropdown-item>
@@ -17,8 +17,29 @@
         </el-dropdown>
       </el-header>
       <el-container>
-        <el-aside>Header</el-aside>
-        <el-main>Main</el-main>
+        <el-aside>
+          <el-menu router>
+            <div
+              v-for="(item, index) in this.$router.options.routes"
+              :key="index"
+            >
+              <el-submenu index="1" v-if="!item.hidden">
+                <template slot="title">
+                  <span>{{ item.name }}</span>
+                </template>
+                <el-menu-item
+                  :index="child.path"
+                  v-for="(child, indexj) in item.children"
+                  :key="indexj"
+                  >{{ child.name }}</el-menu-item
+                >
+              </el-submenu>
+            </div>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -78,14 +99,14 @@ export default {
   cursor: pointer;
 }
 
-  .el-dropdown-link img{
-    width: 48px;
-    height: 48px;
-    border-radius: 50%;
-    margin-left: 10px;
-  }
+.el-dropdown-link img {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  margin-left: 10px;
+}
 
-.el-dropdown-link{
+.el-dropdown-link {
   display: flex;
   align-items: center;
 }
