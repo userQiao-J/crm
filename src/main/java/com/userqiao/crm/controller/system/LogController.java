31d19a029e4ef308d8bdb369c035af3de5259c75
userqiao <userqiao@163.com>
2020-06-26 09:35:59 +0800

    实体类放错位置了

--- src/main/java/com/userqiao/crm/controller/system/LogController.java
+++ src/main/java/com/userqiao/crm/controller/system/LogController.java
@@ -1,9 +1,7 @@
 package com.userqiao.crm.controller.system;
 
 import com.github.pagehelper.PageInfo;
-import com.userqiao.crm.mapper.PageResult;
 import com.userqiao.crm.model.OperLogModel;
-import com.userqiao.crm.model.RespBean;
 import com.userqiao.crm.service.OperationLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/userqiao/crm/model/PageResult.java
+++ src/main/java/com/userqiao/crm/model/PageResult.java
@@ -1,4 +1,4 @@
-package com.userqiao.crm.mapper;
+package com.userqiao.crm.model;
 
 import java.util.List;
 
