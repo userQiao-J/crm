a1168562bcf871ac4dfb7f79fa033b9b641df265
userqiao <userqiao@163.com>
2020-06-28 11:36:24 +0800

    菜单树查询

--- web/crm-web/src/components/sys/basic/MenuManager.vue
+++ web/crm-web/src/components/sys/basic/MenuManager.vue
@@ -1,13 +1,41 @@
 <template>
-    <div>菜单管理</div>
+  <div style="width: 40%">
+    <el-tree
+      node-key="id"
+      ref="tree"
+      :props="defaultProps"
+      :data="allmenus"
+    >
+    </el-tree>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "MenuManager"
+export default {
+  name: "MenuManager",
+  data() {
+    return {
+      allmenus: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    };
+  },
+  mounted() {
+    this.initAllMenus();
+  },
+  methods: {
+    initAllMenus() {
+      this.getRequest("/system/basic/permiss/menus").then(resp => {
+        if (resp.object) {
+          console.log(resp.object);
+          this.allmenus = resp.object;
+        }
+      });
     }
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
